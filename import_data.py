368163505fe7c7932d1c7c0083223b4972573b86
rturquier <remi@Moonlight.local>
2020-10-30 15:52:24 +0100

    Removed profiling commands

--- import_data.py
+++ import_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas_profiling import ProfileReport  # Quick preliminary data exploration
 
 import depute_api
 
@@ -9,5 +8,5 @@ api = depute_api.CPCApi()
 deputies_json = api.parlementaires()
 deputies_df = pd.json_normalize(deputies_json)
 
-profile = ProfileReport(deputies_df, title="Pandas Profiling Report #1")
-profile.to_file("df_profile_report.html")
+
+deputies_df.head()
