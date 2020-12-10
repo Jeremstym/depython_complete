5f126e1a859558bd26fbb354f6ca4b128bad1795
Jérémie Stym-Popper <jeremstym@gmail.com>
2020-12-10 17:01:21 +0100

    Modélisation avec VecCount et TfIdf + hyperparam

--- modelisation.py
+++ modelisation.py
@@ -2,7 +2,10 @@
 """
 Created on Wed Dec  9 12:25:56 2020
 
-Modélisation, plusieurs essais
+Feature engeenering avec CountVectorizer, Pipeline, et HashingVectorizer
+Modélisation, plusieurs essais.
+Évaluation et choix des hyperparamètres
+
 
 @author: Jérémie Stym-Popper
 """
@@ -13,57 +16,126 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-from custom_words import super_liste
-from vectorize_text_dep import countervect, counter_maker, tfidf_maker
-from super_dataframe import gd_df, all_df, LR_df, LFI_df, SOC_df
-
-
-###---- Création d'un Random Forest Classifier------
-
-super_vectorizer = counter_maker(all_df)
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-super_vectorizer = super_vectorizer.reset_index()
+from custom_words import super_liste
+from vectorize_text_dep import  give_lotofwords
+from super_dataframe import gd_df, all_df, LR_df, LFI_df, SOC_df
 
-y = super_vectorizer['groupe']
-X = super_vectorizer.drop('groupe', axis=1)
+
+###---- Création d'un échantillon pour le modèkle ------
+
+counter = CountVectorizer(vocabulary=super_liste).fit_transform(give_lotofwords(gd_df)).toarray()
+weak_features = pd.DataFrame(counter, index=gd_df['groupe'])
+weak_features = weak_features.reset_index() # Pour que 'groupe' devienne la target
+
+y = weak_features['groupe']
+X = weak_features.drop('groupe', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.20)
 
-clf1 = RandomForestClassifier()
+###---- Évaluation des hyperparamètres -----
+
+param_grid = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'max_depth' : [4,5,6,7,8],
+    'criterion' :['gini', 'entropy']
+}
+
+CV_rfc = GridSearchCV(estimator=RandomForestClassifier(), 
+                      param_grid=param_grid, cv=5)
+
+CV_rfc.fit(X_train, y_train)
+opti_param = CV_rfc.best_params_
+
+"""
+La fonction met un temps fou à s'exécuter. Voici les paramètres qu'elle trouve
+{'criterion': 'gini',
+ 'max_depth': 7,
+ 'max_features': 'auto',
+ 'n_estimators': 200}
+Pas la peine de la relancer à chaque fois.
+"""
+
+#--- Modélisation ! -------------
+
+clf1 = RandomForestClassifier(criterion="gini", max_depth=7,
+                              max_features="auto", n_estimators=200)
 clf1.fit(X_train, y_train)
 
 
 y_pred = clf1.predict(X_test)
 print(classification_report(y_test, y_pred), clf1.score(X_test,y_test))
 
-###--- Pareil, mais avec tf-idf----
-
-super_vectorizer = tfidf_maker(all_df)
-
-super_vectorizer = super_vectorizer.reset_index()
-
-y = super_vectorizer['groupe']
-X = super_vectorizer.drop('groupe', axis=1)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.20)
-
-clf1 = RandomForestClassifier()
-clf1.fit(X_train, y_train)
 
 
-y_pred = clf1.predict(X_test)
-print(classification_report(y_test, y_pred), clf1.score(X_test,y_test))
+###--- Pareil, mais avec tf-idf -----------------------
+#---- Création d'un échantillon pour le modèle -------------
+
+
+pipe_idf = make_pipeline(CountVectorizer(vocabulary=super_liste),
+                         TfidfTransformer())
+
+tf_idf_result = pipe_idf.fit_transform(give_lotofwords(gd_df)).toarray()
+
+nos_features = pd.DataFrame(tf_idf_result, index=gd_df['groupe'])
+nos_features = nos_features.reset_index() # Pour que 'groupe' devienne la target
+
+y2 = nos_features['groupe']
+X2 = nos_features.drop('groupe', axis=1)
+
+X_train2, X_test2, y_train2, y_test2 = train_test_split(
+    X2, y2, test_size=0.20)
 
 
 
-### -- Autre essai --------
+###---- Évaluation des hyperparamètres -----
 
+
+param_grid = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'max_depth' : [4,5,6,7,8],
+    'criterion' :['gini', 'entropy']
+}
+
+CV_rfc2 = GridSearchCV(estimator=RandomForestClassifier(), 
+                      param_grid=param_grid, cv=5)
+
+CV_rfc2.fit(X_train2, y_train2)
+opti_param2 = CV_rfc2.best_params_
+
+"""
+La fonction met un temps fou à s'exécuter. Voici les paramètres qu'elle trouve
+{'criterion': 'gini',
+ 'max_depth': 8,
+ 'max_features': 'auto',
+ 'n_estimators': 200}
+Pas la peine de la relancer à chaque fois.
+"""
+# ---- Modélisation ! --------------------
+
+clf2 = RandomForestClassifier(criterion="gini", max_depth=8,
+                              max_features="auto", n_estimators=200)
+
+clf2.fit(X_train2, y_train2)
+
+
+y_pred2 = clf2.predict(X_test2)
+print(classification_report(y_test2, y_pred2), clf2.score(X_test2,y_test2))
+
+
+### -- Autre essai brouillons --------
+"""
 
 drop = gd_df[gd_df['groupe']=='LR'].sample(65)
 gd_df = gd_df.drop(drop.index)
@@ -96,7 +168,7 @@ clf2.predict(X_test[0:2])
 
 y_pred = clf2.predict(X_test)
 print(classification_report(y_test, y_pred))
-
+"""
 
 
 
